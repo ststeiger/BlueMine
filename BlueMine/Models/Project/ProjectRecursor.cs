4ee37efe990b3fbf98094cf66d4f96804b040e95
Me <me@no.spam>
2018-02-06 08:23:20 +0100

    Added Project Recursion refinement

--- BlueMine/Models/Project/ProjectRecursor.cs
+++ BlueMine/Models/Project/ProjectRecursor.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 
 namespace BlueMine.Models.Project
 {
+    
+    
     // Definitely not like this: https://martinsdevblog.blogspot.ch/2013/05/how-to-display-recursive-tree-structure.html
     // or this https://stackoverflow.com/questions/6422895/asp-net-mvc-3-razor-recursive-function 
     // Instead, use DisplayTemplates
@@ -11,20 +13,27 @@ namespace BlueMine.Models.Project
     {
         public Db.T_projects Me;
         protected List<Db.T_projects> m_list;
-        
-        
+        public int Level;
+
+
         public ProjectRecursor(Db.T_projects me, List<Db.T_projects> list)
+            :this(me, list, -1)
+        { } // End Constructor 
+        
+        
+        public ProjectRecursor(Db.T_projects me, List<Db.T_projects> list, int level)
         {
-            Me = me;
-            m_list = list;
-        }
+            this.Me = me;
+            this.m_list = list;
+            this.Level = ++level;
+        } // End Constructor 
         
         
         private static int ProjectSorter(ProjectRecursor previous, ProjectRecursor next)
         {
             return System.StringComparer.InvariantCultureIgnoreCase
                 .Compare(previous.Me.name, next.Me.name);
-        }
+        } // End Function ProjectSorter 
         
         
         public List<ProjectRecursor> RootNodes
@@ -36,14 +45,15 @@ namespace BlueMine.Models.Project
                 for (int i = 0; i < this.m_list.Count; ++i)
                 {
                     if (!this.m_list[i].parent_id.HasValue)
-                        ls.Add(new ProjectRecursor(this.m_list[i], this.m_list));
-                }
+                        ls.Add(new ProjectRecursor(this.m_list[i], this.m_list, this.Level));
+                } // Next i 
 
                 ls.Sort(ProjectSorter);
 
                 return ls;
-            }
-        }
+            } // End Get 
+            
+        } // End Property RootNodes 
         
         
         public List<ProjectRecursor> ChildNodes
@@ -57,15 +67,18 @@ namespace BlueMine.Models.Project
                 for (int i = 0; i < this.m_list.Count; ++i)
                 {
                     if (this.m_list[i].parent_id == this.Me.id)
-                        ls.Add(new ProjectRecursor(this.m_list[i], this.m_list));
-                }
+                        ls.Add(new ProjectRecursor(this.m_list[i], this.m_list, this.Level));
+                } // Next i 
                 
                 ls.Sort(ProjectSorter);
                 
                 return ls;
-            }
-        }
-    }
+            } // End Get 
+            
+        } // End Property ChildNodes 
+        
+        
+    } // End Class ProjectRecursor 
     
     
-}
+} // End Namespace BlueMine.Models.Project
