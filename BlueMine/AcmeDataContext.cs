4ccc292e7a94d722f67ce780ad7e744f2733491c
Me <me@no.spam>
2018-02-12 09:50:19 +0100

    Added model generation

---
+++ BlueMine/AcmeDataContext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace BlueMine
+{
+    
+    public partial class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    
+    
+    public partial class AcmeDataContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"Server=localhost;Integrated Security=true;");
+            }
+        }
+        
+        
+        
+        public DbSet<Person> People { get; set; }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+		    	
+		}
+        
+        
+    }
+    
+    
+}
