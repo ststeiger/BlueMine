e9b07f5fa51d32cbd6bd9bece3759718ed4b48e6
Me <me@no.spam>
2018-02-23 11:53:31 +0100

    Image tag helper barebone

---
+++ BlueMine/Code/MyImageTagHelper.cs
@@ -0,0 +1,32 @@
+﻿
+// namespace BlueMine.Code
+
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+
+namespace AuthoringTagHelpers.TagHelpers
+{
+    
+    
+    // [HtmlTargetElement(Attributes = "bold")]
+    [HtmlTargetElement(tag : "image")]
+    public class MyImageTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            string src = output.Attributes["src"].Value as string;
+            output.Attributes.SetAttribute("src", src + "?&v=123");            
+// output.Attributes.SetAttribute(SrcAttributeName, _fileVersionProvider.AddFileVersionToPath(Src));
+            
+                
+                
+            /*
+            output.Attributes.RemoveAll("bold");
+            output.PreContent.SetHtmlContent("<strong>");
+            output.PostContent.SetHtmlContent("</strong>");
+            */
+        }
+    }
+    
+    
+}
