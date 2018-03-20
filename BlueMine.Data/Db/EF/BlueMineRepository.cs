c59ebb3db4cd927bf1d2d5c7ae506c7c199ec5a1
ststeiger <quandary82@hailmail.net>
2018-03-20 17:21:03 +0100

    CSV-Parser update

--- BlueMine.Data/Db/EF/BlueMineRepository.cs
+++ BlueMine.Data/Db/EF/BlueMineRepository.cs
@@ -1,4 +1,13 @@
 ﻿
+using System.Linq;
+//using System.Collections.Generic;
+//using Microsoft.EntityFrameworkCore;
+//using Microsoft.AspNetCore.Mvc.Rendering;
+
+
+//using BlueMine.Db;
+
+
 namespace BlueMine.Db
 {
 
@@ -59,33 +68,103 @@ namespace BlueMine.Db
             
             return ls;
         }
-        
-        
-        public System.Collections.Generic.List<string> GetValues(string csv)
+
+
+
+
+        private System.Collections.Generic.List<T_custom_values>
+            FetchCustomFieldsValues(string customized_type, int id)
+        {
+            //SELECT* FROM custom_values
+            //WHERE customized_type = 'Issue'
+            //AND customized_id = 1
+
+            System.Collections.Generic.List<T_custom_values> custom_values = (
+               from custom_value in this.m_ctx.custom_values
+               where custom_value.customized_type == customized_type
+               && custom_value.id == id
+               select custom_value
+           ).ToList();
+
+            return custom_values;
+        } // End Function FetchCustomFieldsValues 
+
+
+        public System.Collections.Generic.List<T_custom_values> 
+            GetIssueCustomFieldsValues(int id)
+        {
+            return FetchCustomFieldsValues("Issue", id);
+        } // End Function FetchCustomFieldsValues 
+
+
+        public System.Collections.Generic.List<string> FetchCustomFieldEntries(string fieldName)
+        {
+            System.Collections.Generic.List<string> lsFields = null;
+
+            // SELECT possible_values, is_required, name FROM custom_fields WHERE (1=1)
+            // AND name = 'Kundenname' AND field_format = 'list'
+            string notCSV = (
+                from custom_field in this.m_ctx.custom_fields
+                where custom_field.name == fieldName
+                && custom_field.field_format == "list"
+                select custom_field.possible_values
+            ).Single();
+
+            // notCSV  = "--- - INTERN - \"------\" - BKB - Campus Sursee - Helvetia - Julius Bär - Post - Raiffeisen - Rockwell - RSI - SauterFM - SNB - Sonova (Phonak) - SRGSSR - Swisscom - Swisslife - SwissRe - Wincasa - Zürich";
+
+            if (string.IsNullOrWhiteSpace(notCSV))
+                return new System.Collections.Generic.List<string>();
+
+            int firstNewLine = notCSV.IndexOf("\n");
+            if (firstNewLine != -1)
+            {
+                notCSV = notCSV.Substring(firstNewLine + 1);
+            }
+
+            lsFields = this.GetDashSeparatedValues(notCSV);
+            return lsFields;
+        } // End Function FetchCustomFieldEntries 
+
+
+        private System.Collections.Generic.List<string> GetDashSeparatedValues(string csv)
         {
             System.Collections.Generic.List<string> ls = 
                 new System.Collections.Generic.List<string>();
-            
+
+            if (csv == null)
+                return ls;
+
             using (System.IO.TextReader reader = new System.IO.StringReader(csv))
             {
-                System.Tuple<System.Collections.Generic.IList<string>, 
-                    System.Collections.Generic.IEnumerable<
-                        System.Collections.Generic.IList<string>
-                    >
-                > data = Data.CsvParser.ParseHeadAndTail(reader, '-', '"');
-                
-                foreach (string header in data.Item1)
+                System.Collections.Generic.IEnumerable<System.Collections.Generic.IList<string>> res =
+                    //Data.CsvParser.Parse(csv, '-', '"'); // foo - use ParseSimple
+                    Data.CsvParser.Parse(reader, '-', '"');
+
+                foreach (System.Collections.Generic.IList<string> row in res)
                 {
-                    if (!string.IsNullOrWhiteSpace(header))
-                        ls.Add(header);
-                } // Next header 
-                
+                    string val = row[1];
+                    // if (!string.IsNullOrWhiteSpace(val))
+                    ls.Add(val);
+                } // Next row 
+
+                //System.Tuple<System.Collections.Generic.IList<string>,
+                //    System.Collections.Generic.IEnumerable<
+                //        System.Collections.Generic.IList<string>
+                //    >
+                //> data = Data.CsvParser.ParseHeadAndTail(reader, '-', '"');
+
+                //foreach (string header in data.Item1)
+                //{
+                //    if (!string.IsNullOrWhiteSpace(header))
+                //        ls.Add(header);
+                //} // Next header 
+
             } // End Using reader 
-            
+
             return ls;
-        } // End Function GetValues 
-        
-        
+        } // End Function GetDashSeparatedValues  
+
+
     } // End Class BlueMineRepository 
     
     
--- BlueMine.Data/General/CsvParser.cs
+++ BlueMine.Data/General/CsvParser.cs
@@ -34,6 +34,114 @@ namespace BlueMine.Data
             return HeadAndTail(Parse(reader, delimiter, qualifier));
         }
 
+
+        public static List<List<string>> ParseSimple(string content, char delimiter, char qualifier)
+        {
+            using (var reader = new System.IO.StringReader(content))
+                return ParseSimple(reader, delimiter, qualifier);
+        }
+
+
+        public static List<List<string>> ParseSimple(System.IO.TextReader reader, char delimiter, char qualifier)
+        {
+            List<List<string>> ls = new List<List<string>>();
+
+            bool inQuote = false;
+            List<string> record = new List<string>();
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+
+            while (reader.Peek() != -1)
+            {
+                var readChar = (char)reader.Read();
+
+                if (readChar == '\n' || (readChar == '\r' && (char)reader.Peek() == '\n'))
+                {
+                    // If it's a \r\n combo consume the \n part and throw it away.
+                    if (readChar == '\r')
+                        reader.Read();
+
+                    if (inQuote)
+                    {
+                        if (readChar == '\r')
+                            sb.Append('\r');
+                        sb.Append('\n');
+                    }
+                    else
+                    {
+                        if (record.Count > 0 || sb.Length > 0)
+                        {
+                            record.Add(sb.ToString());
+                            sb.Clear();
+                        }
+
+                        if (record.Count > 0)
+                        {
+                            // yield return record;
+                            ls.Add(record);
+                        }
+
+
+                        record = new List<string>(record.Count);
+                    }
+                }
+                else if (sb.Length == 0 && !inQuote)
+                {
+                    if (readChar == qualifier)
+                        inQuote = true;
+                    else if (readChar == delimiter)
+                    {
+                        record.Add(sb.ToString());
+                        sb.Clear();
+                    }
+                    else if (char.IsWhiteSpace(readChar))
+                    {
+                        // Ignore leading whitespace
+                    }
+                    else
+                        sb.Append(readChar);
+                }
+                else if (readChar == delimiter)
+                {
+                    if (inQuote)
+                        sb.Append(delimiter);
+                    else
+                    {
+                        record.Add(sb.ToString());
+                        sb.Clear();
+                    }
+                }
+                else if (readChar == qualifier)
+                {
+                    if (inQuote)
+                    {
+                        if ((char)reader.Peek() == qualifier)
+                        {
+                            reader.Read();
+                            sb.Append(qualifier);
+                        }
+                        else
+                            inQuote = false;
+                    }
+                    else
+                        sb.Append(readChar);
+                }
+                else
+                    sb.Append(readChar);
+            }
+
+            if (record.Count > 0 || sb.Length > 0)
+                record.Add(sb.ToString());
+
+            if (record.Count > 0)
+            {
+                // yield return record;
+                ls.Add(record);
+            }
+
+            return ls;
+        }
+
+
         public static IEnumerable<IList<string>> Parse(System.IO.TextReader reader, char delimiter, char qualifier)
         {
             var inQuote = false;
