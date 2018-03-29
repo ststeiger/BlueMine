b58c6fdc9027ec88895e7b4c3c1bc84fca422aa1
ststeiger <quandary82@hailmail.net>
2018-03-29 17:35:36 +0200

    Bak-File

--- BlueMine/SQL/load_bak_file.sql
+++ BlueMine/SQL/load_bak_file.sql
@@ -1,34 +1,35 @@
 ﻿
-RESTORE FILELISTONLY
-FROM DISK = 'D:\temp\SQL\COR_Basic_SwissLife_PAV_sts.bak'
+RESTORE FILELISTONLY 
+FROM DISK = 'D:\temp\SQL\COR_Basic_SwissLife_PAV_sts.bak' 
 
-RESTORE HEADERONLY
-FROM DISK = 'D:\temp\SQL\COR_Basic_SwissLife_PAV_sts.bak'
+RESTORE HEADERONLY 
+FROM DISK = 'D:\temp\SQL\COR_Basic_SwissLife_PAV_sts.bak' 
 
--- BackupName -- Backup-Name
--- BackupTypeDescription -- Komponente
--- RecoveryModel -- Typ
+-- BackupName -- Backup-Name 
+-- BackupTypeDescription -- Komponente 
+-- RecoveryModel -- Typ 
 -- ServerName	-- Server 
 -- DatabaseName -- Datenbank 
--- Position
+-- Position 
 
 
--- FirstLSN -- Erste LSN
+-- FirstLSN -- Erste LSN 
 -- LastLSN -- Letzte LSN 
--- CheckpointLSN -- Prüfpunkt-LSN
--- DatabaseBackupLSN -- Vollständige LSN
--- BackupStartDate	 -- Startdatum
+-- CheckpointLSN -- Prüfpunkt-LSN 
+-- DatabaseBackupLSN -- Vollständige LSN 
+-- BackupStartDate	 -- Startdatum 
 -- BackupFinishDate -- Beendigungsdatum 
--- BackupSize -- Grösse
+-- BackupSize -- Grösse 
 -- UserName -- Benutzername 
--- ExpirationDate -- Ablaufdatum
--- Collation
+-- ExpirationDate -- Ablaufdatum 
+-- Collation 
+
 
 /*
-RESTORE DATABASE backup_lookup
- FROM DISK = 'C:\backup.bak'
-WITH REPLACE,
- MOVE 'Old Database Name' TO 'C:\Program Files\Microsoft SQL Server\MSSQL10_50.MSSQLSERVER\MSSQL\DATA\backup_lookup.mdf',
- MOVE 'Old Database Name_log' TO 'C:\Program Files\Microsoft SQL Server\MSSQL10_50.MSSQLSERVER\MSSQL\DATA\backup_lookup_log.ldf'
-GO
+RESTORE DATABASE backup_lookup 
+	FROM DISK = 'C:\backup.bak' 
+WITH REPLACE, 
+	MOVE 'Old Database Name' TO 'C:\Program Files\Microsoft SQL Server\MSSQL10_50.MSSQLSERVER\MSSQL\DATA\backup_lookup.mdf', 
+	MOVE 'Old Database Name_log' TO 'C:\Program Files\Microsoft SQL Server\MSSQL10_50.MSSQLSERVER\MSSQL\DATA\backup_lookup_log.ldf' 
+GO 
 */
