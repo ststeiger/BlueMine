0037c9a1ed3605664351e30a2b716a0606e4594e
Me <me@no.spam>
2018-04-04 09:28:00 +0200

    Trying to fix slickgrid-es6

---
+++ BlueMine/TypeScript/index.ts
@@ -0,0 +1,17 @@
+import Slick from './slick.core';
+import Editors from './slick.editors';
+import Data from './slick.dataview';
+import Grid from './slick.grid';
+import FrozenGrid from './slick-frozen.grid';
+import Formatters from './slick.formatters';
+// import * as Plugins from '../plugins';
+
+export {
+  Slick,
+  Editors,
+  Formatters,
+  Data,
+  Grid,
+  FrozenGrid 
+  // ,Plugins
+}
