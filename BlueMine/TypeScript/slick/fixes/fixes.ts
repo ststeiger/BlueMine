5b62c69461fbf60ef3ecf239d8788a569e947ec5
ststeiger <quandary82@hailmail.net>
2018-04-06 18:33:59 +0200

    AutoBind

--- BlueMine/TypeScript/slick/fixes/fixes.ts
+++ BlueMine/TypeScript/slick/fixes/fixes.ts
@@ -1,4 +1,24 @@
 
+
+// this.autoBind(this);
+export function autoBind(self: any)
+{
+    for (const key of Object.getOwnPropertyNames(self.constructor.prototype))
+    {
+        const val = self[key];
+
+        if (key !== 'constructor' && typeof val === 'function')
+        {
+            // console.log(key);
+            self[key] = val.bind(self);
+        } // End if (key !== 'constructor' && typeof val === 'function') 
+
+    } // Next key 
+
+    return self;
+} // End Function autoBind
+
+
 // https://stackoverflow.com/questions/38077164/es6-export-from-import
 
 
