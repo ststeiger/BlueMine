e4cf2aff337b9aa48fb7b13e223ede3dc6c1a98c
Me <me@no.spam>
2018-06-04 20:40:38 +0200

    Fix stdlib...

--- SharpenCore/Sharpen.Unix/Mono.Posix/Mono.Unix.Native/Stdlib.cs
+++ SharpenCore/Sharpen.Unix/Mono.Posix/Mono.Unix.Native/Stdlib.cs
@@ -367,7 +367,8 @@ namespace Mono.Unix.Native {
 	//
 	public class Stdlib
 	{
-		internal const string LIBC = "msvcrt";
+		// internal const string LIBC = "msvcrt";
+		internal const string LIBC = "c";
 		internal const string MPH  = "MonoPosixHelper";
 
 		internal Stdlib () {}
